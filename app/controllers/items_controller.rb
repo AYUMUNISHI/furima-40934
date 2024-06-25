7f0e5f9fcb54182ff43b0bbbc4f05371f64e9c6a
AYUMUNISHI <a63yu3m6@gmail.com>
2024-06-25 18:02:07 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:user)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end 
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product,:image, :description, :category_id, :status_id, :freight_id, :prefecture_id, :shipping_date_id, :price, :user).merge(user_id: current_user.id)
+end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,23 @@
+class Item < ApplicationRecord
+ belongs_to :user
+#  has_one :order
+ has_one_attached :image
+ extend ActiveHash::Associations::ActiveRecordExtensions
+ belongs_to :category
+ belongs_to :status
+ belongs_to :freight
+ belongs_to :prefecture
+ belongs_to :shipping_date
+ 
+
+
+
+ validates :product, :description, :price, :image, presence: true 
+ validates :category_id, :status_id, :freight_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 } 
+ validates :price, format: { with: /\A[ -~｡-ﾟ]*\z/, message: "は半角文字のみで入力してください" }
+ validates :price, numericality: {
+  only_integer: true,
+  greater_than_or_equal_to: 300,
+  less_than_or_equal_to: 9999999
+}
+end
